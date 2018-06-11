cc22f101adfb794e1fce4333ad5b435798e54775
JanB3072 <1506741250@qq.com>
2018-06-11 21:49:43 +0800

    更新

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -14,47 +14,6 @@ gulp.task("sass",function(){
 	.pipe(minifyCss())
 	.pipe(gulp.dest("./dist/css"));
 });
-
-//定义一个复制文件的任务（命令）
-// gulp.task("copyfile",function(){
-// 	//gulp.src("index.html").pipe(gulp.dest("dist"));
-// 	gulp.src("index.html").pipe(gulp.dest("dist"));
-// });
-
-//复制图片文件
-// gulp.task("images",function(){
-// 	gulp.src("img/*.jpg")
-// 	.pipe(gulp.dest("dist\\img"));
-// });
-
-//合并文件
-// gulp.task("concatjs",function(){
-// 	gulp.src(["js/index.js","js/goodslist.js"])
-// 	.pipe(concat("common.js"))
-// 	.pipe(gulp.dest("dist\\js"));
-// });
-
-
-//合并和压缩文件
-// gulp.task("concatanduglifyjs",function(){
-// 	gulp.src(["js/index.js","js/goodslist.js"])
-// 	.pipe(concat("common.js"))
-// 	.pipe(uglify())
-// 	.pipe(gulp.dest("dist\\js"));
-// });
-
-
-
-//合并和压缩重命名文件
-// gulp.task("concatanduglifyandrenamejs",function(){
-// 	gulp.src(["js/index.js","js/goodslist.js"])
-// 	.pipe(concat("common.js"))
-// 	.pipe(gulp.dest("dist\\js"))
-// 	.pipe(uglify())
-// 	.pipe(rename("common.min.js"))
-// 	.pipe(gulp.dest("dist\\js"));
-// });
-
 //合并和压缩重命名文件
 gulp.task("babel",function(){
 	gulp.src("./src/js/*.js")
@@ -67,15 +26,4 @@ gulp.task("babel",function(){
 gulp.task("watchall",function(){
 	gulp.watch("./src/style/*.scss",["sass"]);
 	gulp.watch("./src/js/*.js",["babel"]);
-	// gulp.watch("index.html",["copyfile"]);
-	// gulp.watch("img/*.jpg",["images"]);
-	// gulp.watch(["js/index.js","js/goodslist.js"],["concatanduglifyandrenamejs"]);
 });
-
-
-//简易的web服务器
-// gulp.task("server",function(){
-// 	connect.server({
-// 		"root":"dist"
-// 	});
-// });
--- project/src/js/DetailPages.js
+++ project/src/js/DetailPages.js
@@ -6,7 +6,6 @@ $(".navCen a").each(function(k,v){
 			$(n).show();
 			$(".trigon").each(function(x,y){
 				if(k==x){
-					console.log(y);
 					$(y).show();
 				};
 			});
@@ -35,3 +34,38 @@ $(".navCen a").each(function(k,v){
 		});
 	});
 });
+	$(".imgsBox li").each(function(k,v){
+		$(v).mousemove(function(){
+			$(".mirrorBox").css("backgroundImage","url(img/fiveb"+(k+1)+".jpg)");
+			$(".bigBox").css("backgroundImage","url(img/fiveb"+(k+1)+".jpg)");
+		});
+	});
+	$(".mirrorBox").mouseover(function(){
+		$(".bigBox").css("display","block");
+		$(".smallBox").css("display","block");
+	});
+	
+	$(".mirrorBox").mouseout(function(){
+		$(".bigBox").css("display","none");
+		$(".smallBox").css("display","none");
+	});
+	
+	$(".mirrorBox")[0].onmousemove=function(event){
+		let evt = event || window.event;
+		//1、数据
+		let left1 = evt.pageX-$(".mirrorBox")[0].offsetLeft-$(".smallBox")[0].offsetWidth/2;
+		let top1 =  evt.pageY-$(".mirrorBox")[0].offsetTop-$(".smallBox")[0].offsetHeight/2;
+		if(left1<0){
+			left1=0;
+		}else if(left1>(450-$(".smallBox")[0].offsetWidth)){
+			left1 = 450-$(".smallBox")[0].offsetWidth;
+		};
+		if(top1<0){
+			top1=0;
+		}else if(top1>(450-$(".smallBox")[0].offsetHeight)){
+			top1 = 450-$(".smallBox")[0].offsetHeight;
+		};
+		$(".smallBox")[0].style.left=left1+"px";
+		$(".smallBox")[0].style.top=top1+"px";
+		$(".bigBox")[0].style.backgroundPosition = (-2*left1)+"px "+(-2*top1)+"px";
+	};
